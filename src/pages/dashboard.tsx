0cb4f4d674b0cc8d9d4421e3ce638ca135c6bb84
guilermmm <guilherme.r.a.melo@gmail.com>
2023-04-03 05:21:46 -0300

    change status button on user card

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,8 @@
 import type { Profile, User } from "@prisma/client";
 import type { GetServerSidePropsContext } from "next";
 import { signOut, useSession } from "next-auth/react";
-import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDebounce } from "use-debounce";
 import AdminNavbar from "../components/AdminNavbar";
 import ErrorPage from "../components/ErrorPage";
@@ -14,6 +13,9 @@ import MagnifyingGlassIcon from "../components/icons/MagnifyingGlassIcon";
 import { env } from "../env/server.mjs";
 import { getServerAuthSession } from "../server/auth";
 import { api } from "../utils/api";
+import XCircleIcon from "../components/icons/XCircleIcon";
+import CheckCircleIcon from "../components/icons/CheckCircleIcon";
+import { classList } from "../utils";
 
 const Dashboard = () => {
   const { data: session } = useSession();
@@ -32,13 +34,7 @@ const Dashboard = () => {
     <div className="flex h-full flex-col bg-slate-100">
       <div className="flex items-center justify-between bg-gold-500 p-2">
         <div className="flex items-center">
-          <Image
-            width={64}
-            height={64}
-            src={session?.user.image ?? ""}
-            alt="Foto de perfil"
-            className="rounded-full"
-          />
+          <ProfilePic user={session?.user} size="sm" />
           <h1 className="ml-4 text-lg font-medium text-blue-700">
             Olá, <span className="font-bold">{session?.user.name}</span>!
           </h1>
@@ -68,7 +64,11 @@ const Dashboard = () => {
           ) : (
             <div className="flex flex-col flex-wrap items-stretch gap-2 sm:flex-row">
               {profiles.data.map(profile => (
-                <UserCard key={profile.id} profile={profile} />
+                <UserCard
+                  key={profile.id}
+                  profile={profile}
+                  refetch={async () => void (await profiles.refetch())}
+                />
               ))}
             </div>
           )}
@@ -79,20 +79,106 @@ const Dashboard = () => {
   );
 };
 
-const UserCard = ({ profile }: { profile: Profile & { user: User | null } }) => {
+const UserCard = ({
+  profile,
+  refetch,
+}: {
+  profile: Profile & { user: User | null };
+  refetch: () => Promise<void>;
+}) => {
   return (
-    <Link
-      href={`/manage/${profile.id}`}
-      className="flex max-w-[calc(100vw_-_2rem)] flex-1 flex-row items-center rounded-md bg-slate-50 p-3 shadow-md transition-shadow hover:shadow-xl"
+    <div
+      className={`flex max-w-[calc(100vw_-_2rem)] flex-1 flex-row items-center justify-between rounded-md border-r-4 bg-slate-50 shadow-md transition-shadow hover:shadow-xl`}
     >
-      <div className="flex h-12 w-12 items-center justify-center rounded-full bg-white">
-        <ProfilePic user={profile.user} size="sm" />
+      <Link href={`/manage/${profile.id}`} className="flex grow items-center truncate p-3 pr-0">
+        <div className="flex h-12 w-12 items-center justify-center rounded-full bg-white">
+          <ProfilePic user={profile.user} size="sm" />
+        </div>
+        <div className="ml-4 truncate">
+          <div className="truncate text-lg font-medium text-slate-800">{profile.user?.name}</div>
+          <div className="truncate text-sm text-slate-500">{profile.email}</div>
+        </div>
+      </Link>
+      <StatusButton isActive={profile.isActive} id={profile.id} refetch={refetch} />
+    </div>
+  );
+};
+
+const StatusButton = ({
+  isActive,
+  id,
+  refetch,
+}: {
+  isActive: boolean;
+  id: string;
+  refetch: () => Promise<void>;
+}) => {
+  const [opened, setOpened] = useState(false);
+  const changeStatus = isActive
+    ? api.user.deactivate.useMutation()
+    : api.user.activate.useMutation();
+
+  useEffect(() => {
+    const second = setTimeout(() => {
+      setOpened(false);
+    }, 3000);
+
+    return () => {
+      clearTimeout(second);
+    };
+  }, [opened]);
+
+  return (
+    <div
+      className={classList("group h-full transition-all", {
+        "pl-0": opened,
+        "pl-8": !opened,
+      })}
+      onClick={() => setOpened(true)}
+    >
+      <div
+        className={classList(
+          "flex h-full items-center justify-center overflow-x-hidden rounded-r-md transition-all",
+          {
+            "w-[72px] rounded-md": opened,
+            "w-3": !opened,
+            "bg-green-500": isActive,
+            "bg-red-500": !isActive,
+          },
+        )}
+      >
+        <button
+          className="m-3"
+          onClick={() =>
+            changeStatus.mutate(
+              { profileId: id },
+              {
+                onSuccess: () => {
+                  void refetch();
+                  setOpened(false);
+                },
+              },
+            )
+          }
+        >
+          {isActive ? (
+            <XCircleIcon
+              className={classList("text-white transition-all", {
+                "h-0 w-0": !opened,
+                "h-10 w-10": opened,
+              })}
+            />
+          ) : (
+            <CheckCircleIcon
+              className={classList("text-white transition-all", {
+                "h-0 w-0": !opened,
+                "h-10 w-10": opened,
+              })}
+            />
+          )}
+        </button>
       </div>
-      <div className="ml-4 truncate">
-        <div className="truncate text-lg font-medium text-slate-800">{profile.user?.name}</div>
-        <div className="truncate text-sm text-slate-500">{profile.email}</div>
-      </div>
-    </Link>
+    </div>
   );
 };
 
