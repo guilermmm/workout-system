7b91b1a5932141b80e08a33274331f3e9d753195
guilermmm <guilherme.r.a.melo@gmail.com>
2023-05-31 14:31:19 -0300

    build

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,23 +1,5 @@
-import dynamic from "next/dynamic";
-import BasicDocument, { profilePDF, workoutsPDF } from "../utils/pdf";
-
-export const DownloadButton = dynamic(
-  async () => (await import("@react-pdf/renderer")).PDFDownloadLink,
-  {
-    ssr: false,
-  },
-);
-
 const Test = () => {
-  return (
-    <DownloadButton
-      className="rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700"
-      document={<BasicDocument profile={profilePDF} workouts={workoutsPDF} />}
-      fileName="somename.pdf"
-    >
-      asdasd
-    </DownloadButton>
-  );
+  return <div>test</div>;
 };
 
 export default Test;
